476c4e7dbdb543b78083879c6e816c5e9374f2a4
Rafael Barbosa <rafaelbarbosatec@gmail.com>
2022-02-15 20:09:06 -0300

    finaliza layout da tela de login

--- lib/view/sign_in/sign_in_page.dart
+++ lib/view/sign_in/sign_in_page.dart
@@ -11,7 +11,42 @@ class _SignInPageState extends State<SignInPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Hello'),
+      appBar: AppBar(
+        title: const Text('Login'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(50),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Senha',
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Entrar'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
